4bd7433f09a0e92ad902b24c3d110b2a2a3182e6
Pranay Gupta <64855541+thepranaygupta@users.noreply.github.com>
2021-06-13 22:36:22 +0530

    BST complete

--- 04. Tree/02. Binary Search Tree & Implementation/BinarySearchTree.java
+++ 04. Tree/02. Binary Search Tree & Implementation/BinarySearchTree.java
@@ -11,6 +11,9 @@ public class BinaryTree {
 		Scanner sc = new Scanner(System.in);
 		BST bt = new BST();
 		boolean flag = true;
+		int inorder[];
+		int preorder[];
+		int postorder[];
 		while (flag) {
 			int val = 0;
 			System.out.println();
@@ -35,80 +38,98 @@ public class BinaryTree {
 			System.out.println("19 - Diameter of Binary Tree (O(n^2) Approach)");
 			System.out.println("20 - Diameter of Binary Tree (O(n) Approach)");
 			System.out.println("21 - Delete a Node");
-//			System.out.println("3  - Delete a Node");
+			System.out.println("22 - Construct BT from Pre-Order and In-Order");
+			System.out.println("23 - Construct BT from Post-Order and In-Order");
+			System.out.println("24 - Construct BST from In-Order");
+			System.out.println("25 - Construct BST from Pre-Order");
+			System.out.println("26 - Construct BST from Post-Order");
 			System.out.println("0  - Exit");
 
 			int ch = sc.nextInt();
 			switch (ch) {
-			case 0:
-				flag = false;
-				break;
+
 			case 1:
 				System.out.print("Enter Value to Insert: ");
 				val = sc.nextInt();
 				bt.insertIterative(val);
 				break;
+
 			case 2:
 				System.out.print("Enter Value to Insert: ");
 				val = sc.nextInt();
 				bt.root = bt.insertRecursive(bt.root, val);
 				break;
+
 			case 3:
 				System.out.println("Enter Value to Insert: ");
 				val = sc.nextInt();
 				bt.insertLevelOrder(bt.root, val);
 				break;
+
 			case 4:
 				bt.print2D(bt.root, GLOBALSPACE);
 				break;
+
 			case 5:
 				System.out.println("Height of the Tree is = " + bt.height(bt.root));
 				break;
+
 			case 6:
 				System.out.println("Sum of all the Nodes in the Tree is = " + bt.sumOfNodes(bt.root));
 				break;
+
 			case 7:
 				System.out.println("Total Number of Nodes in the Tree is = " + bt.countNodes(bt.root));
 				break;
+
 			case 8:
 				System.out.println("The Maximum Value of the Binary Tree is = " + bt.maxValue(bt.root));
 				break;
+
 			case 9:
 				System.out.println("The Binary Tree in Level Order/Breadth First Search (using Queue) is = ");
 				bt.printLevelOrderQueue(bt.root);
 				break;
+
 			case 10:
 				System.out.println("The Binary Tree in Level Order/Breadth First Search (using Recursion) is = ");
 				bt.printLevelOrderRecursion(bt.root);
 				break;
+
 			case 11:
 				System.out.print("Enter the Level to print Level Order: ");
 				int l = sc.nextInt();
 				System.out.println("The Nodes in Level-" + l + " in Level Order are:");
 				bt.printGivenLevel(bt.root, l);
 				break;
+
 			case 12:
 				System.out.print("Enter Level(K): ");
 				val = sc.nextInt();
 				bt.sum = 0;
 				System.out.println("Sum of values at level K is = " + bt.sumAtK(bt.root, val));
 				break;
+
 			case 13:
 				System.out.println("The Tree Nodes in Pre-Order Fashion = ");
 				bt.printPreOrder(bt.root);
 				break;
+
 			case 14:
 				System.out.println("The Tree Nodes in In-Order Fashion = ");
 				bt.printInOrder(bt.root);
 				break;
+
 			case 15:
 				System.out.println("The Tree Nodes in Post-Order Fashion = ");
 				bt.printPostOrder(bt.root);
 				break;
+
 			case 16:
 				System.out.println("All Paths from Root to Leaf Nodes are = ");
 				bt.rootToLeaf(bt.root);
 				break;
+
 			case 17:
 				System.out.print("Enter the Value to Search: ");
 				val = sc.nextInt();
@@ -118,6 +139,7 @@ public class BinaryTree {
 					System.out.println("Value Not Found");
 //				System.out.println(bt.iterativeSearch(val));
 				break;
+
 			case 18:
 				System.out.print("Enter the Value to Search: ");
 				val = sc.nextInt();
@@ -127,18 +149,85 @@ public class BinaryTree {
 					System.out.println("Value Found");
 //				System.out.println(bt.iterativeSearch(val));
 				break;
+
 			case 19:
 				System.out.println("Diameter of Binary Tree (O(n^2) Approach) is = " + bt.diameterOfBT(bt.root));
 				break;
+
 			case 20:
 				DiaPair d = new DiaPair();
 				d = bt.diameter(bt.root);
 				System.out.println("Diameter of Binary Tree (O(n) Approach) is = " + d.dia);
 				break;
+
 			case 21:
 				System.out.print("Enter Value to Delete: ");
 				val = sc.nextInt();
 				bt.root = bt.delete(bt.root, val);
+				break;
+
+			case 22:
+				System.out.print("Enter Number of Nodes in Binary Tree: ");
+				val = sc.nextInt();
+				preorder = new int[val];
+				inorder = new int[val];
+
+				System.out.println("Enter Values in Pre-Order Sequence:");
+				for (int i = 0; i < val; i++)
+					preorder[i] = sc.nextInt();
+				System.out.println("Enter Values in In-Order Sequence:");
+				for (int i = 0; i < val; i++)
+					inorder[i] = sc.nextInt();
+				bt.root = bt.buildTreeFromInorderPreorder(preorder, inorder);
+				break;
+
+			case 23:
+				System.out.print("Enter Number of Nodes in Binary Tree: ");
+				val = sc.nextInt();
+				postorder = new int[val];
+				inorder = new int[val];
+
+				System.out.println("Enter Values in Post-Order Sequence:");
+				for (int i = 0; i < val; i++)
+					postorder[i] = sc.nextInt();
+				System.out.println("Enter Values in In-Order Sequence:");
+				for (int i = 0; i < val; i++)
+					inorder[i] = sc.nextInt();
+				bt.root = bt.buildTreeFromInorderPostorder(postorder, inorder);
+				break;
+
+			case 24:
+				System.out.print("Enter Number of Nodes in Binary Search Tree: ");
+				val = sc.nextInt();
+				inorder = new int[val];
+				System.out.println("Enter Values in In-Order Sequence:");
+				for (int i = 0; i < val; i++)
+					inorder[i] = sc.nextInt();
+				bt.root = bt.buildBSTfromInorder(inorder);
+				break;
+
+			case 25:
+				System.out.print("Enter Number of Nodes in Binary Search Tree: ");
+				val = sc.nextInt();
+				preorder = new int[val];
+				System.out.println("Enter Values in Pre-Order Sequence:");
+				for (int i = 0; i < val; i++)
+					preorder[i] = sc.nextInt();
+				bt.root = bt.buildBSTfromPreorder(preorder);
+				break;
+
+			case 26:
+				System.out.print("Enter Number of Nodes in Binary Search Tree: ");
+				val = sc.nextInt();
+				postorder = new int[val];
+				System.out.println("Enter Values in Post-Order Sequence:");
+				for (int i = 0; i < val; i++)
+					postorder[i] = sc.nextInt();
+				bt.root = bt.buildBSTfromPostorder(postorder);
+				break;
+
+			case 0:
+				flag = false;
 				break;
 			}
 		}
@@ -238,6 +327,7 @@ class BST {
 			root = rt;
 			return;
 		}
+		
 		Queue<Node> q = new LinkedList<Node>();
 		q.add(rt);
 
@@ -250,12 +340,14 @@ class BST {
 				System.out.println("Value inserted at the left.");
 				root = rt;
 				return;
-			} else if (n.right == null) {
+			} 
+			else if (n.right == null) {
 				n.right = nn;
 				System.out.println("Value inserted at the right.");
 				root = rt;
 				return;
-			} else {
+			} 
+			else {
 				q.add(n.left);
 				q.add(n.right);
 			}
@@ -265,7 +357,6 @@ class BST {
 
 // START 4 - Print 2D
 	public void print2D(Node r, int space) {
-
 		if (r == null) // base case
 			return;
 
@@ -273,6 +364,7 @@ class BST {
 		System.out.println();
 		for (int i = BinaryTree.GLOBALSPACE; i < space; i++)
 			System.out.print(" ");
+
 		System.out.println(r.data);
 		print2D(r.left, space + BinaryTree.GLOBALSPACE);
 	}
@@ -282,6 +374,7 @@ class BST {
 	public static int height(Node r) {
 		if (r == null)
 			return -1;
+
 		else {
 			int lheight = height(r.left);
 			int rheight = height(r.right);
@@ -315,8 +408,10 @@ class BST {
 
 // START 8 - Maximum Value in the Binary Tree
 	public int maxValue(Node r) {
-		if (r == null)
+		if (r == null) {
 			return Integer.MIN_VALUE;
+		}
+
 		int lmax = maxValue(r.left); // find the Maximum Value in the left subtree
 		int rmax = maxValue(r.right); // find the Maximum Value in the right subtree
 		return Math.max(r.data, Math.max(lmax, rmax));
@@ -328,13 +423,16 @@ class BST {
 		Node temp = null;
 		Queue<Node> queue = new LinkedList<Node>();
 		queue.add(r);
+
 		while (queue.size() > 0) {
 			temp = queue.poll(); // store the value of current node in 'temp' and dequeue it
 			System.out.print(temp.data + " "); // print the current node value
-			if (temp.left != null)
+			if (temp.left != null) {
 				queue.add(temp.left); // enqueue the left child
-			if (temp.right != null)
+			}
+			if (temp.right != null) {
 				queue.add(temp.right); // enqueue the right child
+			}
 		}
 	}
 // END 9 - Print Level Order/Breadth First Search (using queue)
@@ -351,8 +449,10 @@ class BST {
 	public void printGivenLevel(Node r, int level) {
 		if (r == null) // base case
 			return;
+
 		else if (level == 0)
 			System.out.print(r.data + " ");
+
 		else { // level > 0
 			printGivenLevel(r.left, level - 1);
 			printGivenLevel(r.right, level - 1);
@@ -366,8 +466,10 @@ class BST {
 	public int sumAtK(Node r, int K) {
 		if (r == null)
 			return sum;
+
 		else if (K == 0) // if K is at root level (0)
 			sum = sum + r.data;
+
 		else {
 			sumAtK(r.left, K - 1);
 			sumAtK(r.right, K - 1);
@@ -380,6 +482,7 @@ class BST {
 	public void printPreOrder(Node r) { // N L R
 		if (r == null)
 			return;
+
 		System.out.print(r.data + " ");
 		printPreOrder(r.left);
 		printPreOrder(r.right);
@@ -390,6 +493,7 @@ class BST {
 	public void printInOrder(Node r) { // L N R
 		if (r == null)
 			return;
+
 		printInOrder(r.left);
 		System.out.print(r.data + " ");
 		printInOrder(r.right);
@@ -400,6 +504,7 @@ class BST {
 	public void printPostOrder(Node r) { // L R N
 		if (r == null)
 			return;
+
 		printPostOrder(r.left);
 		printPostOrder(r.right);
 		System.out.print(r.data + " ");
@@ -413,10 +518,14 @@ class BST {
 	public static void rootToLeaf(Node r) {
 		if (r == null)
 			return;
+
 		st.push(r.data);
 		rootToLeaf(r.left);
 		if (r.left == null && r.right == null) // if the encountered node is a leaf node
+		{
 			System.out.println(st);
+		}
+
 		rootToLeaf(r.right);
 		st.pop();
 	}
@@ -435,8 +544,11 @@ class BST {
 				if (val == temp.data) { // if the node is found
 					flag = true;
 					break;
-				} else if (val < temp.data) // traverse to the left-subtree
+				}
+
+				else if (val < temp.data) // traverse to the left-subtree
 					temp = temp.left;
+
 				else if (val > temp.data) // traverse to the right-subtree
 					temp = temp.right;
 			}
@@ -452,8 +564,10 @@ class BST {
 
 		if (r.data == val) // if the value is found
 			return r;
+
 		if (r.data > val) // when data>val then traverse to the left-subtree
 			return recursiveSearch(r.left, val);
+
 		return recursiveSearch(r.right, val); // when data<val then traverse to the right-subtree
 	}
 // END 18 - Search in Binary Search Tree(Recursive Approach)
@@ -462,10 +576,13 @@ class BST {
 	public static int diameterOfBT(Node r) {
 		if (r == null)
 			return 0;
+
 		// maximum distance between two nodes of LHS (factor 1)
 		int ld = diameterOfBT(r.left);
+
 		// maximum distance between two nodes of RHS (factor 2)
 		int rd = diameterOfBT(r.right);
+
 		// maximum distance between left's deepest & right's deepest nodes (factor 3)
 		int des = height(r.left) + height(r.right) + 2;
 
@@ -541,4 +658,125 @@ class BST {
 		return node;
 	}
 // END 21 - Delete a Node
+
+// START 22 - Construct BT from Pre-Order and In-Order
+	public Node buildTreeFromInorderPreorder(int preorder[], int inorder[]) {
+		int n = inorder.length;
+		System.out.println("Your BT is Ready PRESS 4 AND ENTER to View");
+		return preInTree(preorder, 0, n - 1, inorder, 0, n - 1);
+	}
+
+	// psi = preorder starting index
+	// pei = preorder ending index.
+	// isi = inorder starting index
+	// iei = inorder ending inex.
+	public Node preInTree(int pre[], int psi, int pei, int in[], int isi, int iei) {
+		if (isi > iei)
+			return null;
+
+		Node n = new Node(pre[psi]);
+		int idx = isi;
+		while (in[idx] != pre[psi])
+			idx++;
+		int tnel = idx - isi; // total no. of element on left side/right side of root.
+
+		n.left = preInTree(pre, psi + 1, psi + tnel, in, isi, idx - 1);
+		n.right = preInTree(pre, psi + tnel + 1, pei, in, idx + 1, iei);
+
+		return n;
+	}
+// END 22 - Construct BT from Pre-Order and In-Order
+
+// START 23 - Construct BT from Post-Order and In-Order
+	public Node buildTreeFromInorderPostorder(int postorder[], int inorder[]) {
+		int n = postorder.length;
+		System.out.println("Your BT is Ready PRESS 4 AND ENTER to View");
+		return postInTree(postorder, 0, n - 1, inorder, 0, n - 1);
+	}
+
+	// psi = postorder starting index
+	// pei = postorder ending index.
+	// isi = inorder starting index
+	// iei = inorder ending inex.
+	public Node postInTree(int post[], int psi, int pei, int in[], int isi, int iei) {
+		if (isi > iei)
+			return null;
+		Node n = new Node(post[pei]);
+		int idx = isi;
+		while (in[idx] != post[pei])
+			idx++;
+		int tnel = idx - isi; // total no of elements on left side/right side of root.
+
+		n.left = postInTree(post, psi, psi + tnel - 1, in, isi, idx - 1);
+		n.right = postInTree(post, psi + tnel, pei - 1, in, idx + 1, iei);
+
+		return n;
+	}
+// END 23 - Construct BT from Post-Order and In-Order
+
+// START 24 - Construct BST from In-Order
+	public Node buildBSTfromInorder(int inorder[]) {
+		int n = inorder.length;
+		System.out.println("Your BST is Ready PRESS 4 AND ENTER to View");
+		return buildBSTfromInorder(inorder, 0, n - 1);
+	}
+
+	private Node buildBSTfromInorder(int[] in, int si, int ei) {
+		if (si > ei)
+			return null;
+		int midRoot = (si + ei) / 2;
+		Node n = new Node(in[midRoot]);
+
+		n.left = buildBSTfromInorder(in, si, midRoot - 1);
+		n.right = buildBSTfromInorder(in, midRoot + 1, ei);
+
+		return n;
+	}
+// END 24 - Construct BST from In-Order
+
+// START 24 - Construct BST from Pre-Order	
+	int idx = 0;
+
+	public Node buildBSTfromPreorder(int preorder[]) {
+		int lr = -1000; // left range
+		int rr = 1000; // right range
+		idx = 0;
+		System.out.println("Your BST is ready PRESS 4 AND ENTER to view");
+		return buildBSTfromPreorder(preorder, lr, rr);
+	}
+
+	private Node buildBSTfromPreorder(int[] pre, int lr, int rr) {
+		if (idx >= pre.length || pre[idx] < lr || pre[idx] > rr)
+			return null;
+
+		Node n = new Node(pre[idx++]);
+
+		n.left = buildBSTfromPreorder(pre, lr, n.data);
+		n.right = buildBSTfromPreorder(pre, n.data, rr);
+
+		return n;
+	}
+// END 24 - Construct BST from Pre-Order	
+
+// START 24 - Construct BST from Post-Order
+	public Node buildBSTfromPostorder(int postorder[]) {
+		int lr = -1000; // left range
+		int rr = 1000; // right range
+		idx = postorder.length - 1;
+		System.out.println("Your BST is Ready PRESS 4 AND ENTER to view");
+		return buildBSTfromPostorder(postorder, lr, rr);
+	}
+
+	private Node buildBSTfromPostorder(int[] post, int lr, int rr) {
+		if (idx < 0 || post[idx] < lr || post[idx] > rr)
+			return null;
+
+		Node n = new Node(post[idx--]);
+
+		n.right = buildBSTfromPostorder(post, n.data, rr);
+		n.left = buildBSTfromPostorder(post, lr, n.data);
+
+		return n;
+	}
+// END 24 - Construct BST from Post-Order		
 }
