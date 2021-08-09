903ee10f683d4a49ee97a91d6dfdec6cc98f7bb0
Pranay Gupta <64855541+thepranaygupta@users.noreply.github.com>
2021-08-09 21:07:45 +0530

    Bubble Sort

---
+++ 02. Algorithms/01. Arrays/00. Sorting/01. BubbleSort/BubbleSort.java
@@ -0,0 +1,27 @@
+// TODO Sort an Array using Bubble-Sort Algorithm
+package array.sorting;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		int arr[] = { 4, 21, 1, 9, -12, 5, 32, 2, 6 };
+		System.out.println("Array before Sorting: " + Arrays.toString(arr));
+
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i++) {
+			boolean swapped = false;
+			for (int j = 0; j < n - i - 1; j++) {
+				if (arr[j + 1] < arr[j]) {
+					swapped = true;
+					int temp = arr[j];
+					arr[j] = arr[j+1];
+					arr[j+1] = temp;
+				}
+			}
+			if (!swapped)
+				break;
+		}
+		System.out.println("Array after Sorting: " + Arrays.toString(arr));
+	}
+}
