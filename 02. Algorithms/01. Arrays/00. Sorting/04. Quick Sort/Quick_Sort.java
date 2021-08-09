11dc2d36f26d7081d2adead82ac7edfca5d9892d
Pranay Gupta <64855541+thepranaygupta@users.noreply.github.com>
2021-08-10 01:58:55 +0530

    Quick Sort

---
+++ 02. Algorithms/01. Arrays/00. Sorting/04. Quick Sort/Quick_Sort.java
@@ -0,0 +1,16 @@
+// TODO: Sort an Array using Quick Sort Technique
+package array.sorting;
+
+import java.util.Arrays;
+
+public class Quick_Sort {
+
+	public static void main(String[] args) {
+		int arr[] = { 4, 21, 1, 9, -12, 5, 32, 2, 6 };
+		System.out.println("Array before Sorting: " + Arrays.toString(arr));
+
+		int n = arr.length;
+		
+		System.out.println("Array after Sorting: " + Arrays.toString(arr));
+	}
+}
