35d021207264563e112f3e552df0fef7d2f50afa
Pranay Gupta <64855541+thepranaygupta@users.noreply.github.com>
2021-08-09 21:37:42 +0530

    Update BubbleSort.java

--- 02. Algorithms/01. Arrays/00. Sorting/01. BubbleSort/BubbleSort.java
+++ 02. Algorithms/01. Arrays/00. Sorting/01. BubbleSort/BubbleSort.java
@@ -11,7 +11,7 @@ public class BubbleSort {
 		int n = arr.length;
 		for (int i = 0; i < n - 1; i++) {
 			boolean swapped = false;
-			for (int j = 0; j < n - i - 1; j++) {
+			for (int j = 0; j < n - 1 - i; j++) {
 				if (arr[j + 1] < arr[j]) {
 					swapped = true;
 					int temp = arr[j];
