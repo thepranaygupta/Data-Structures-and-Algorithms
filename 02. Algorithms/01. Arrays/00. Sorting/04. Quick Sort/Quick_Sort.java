ca91c28514a9c80d7be3585ba2a1079c33aed0a7
Pranay Gupta <64855541+thepranaygupta@users.noreply.github.com>
2021-08-11 20:33:57 +0530

    Quick Sort

--- 02. Algorithms/01. Arrays/00. Sorting/04. Quick Sort/Quick_Sort.java
+++ 02. Algorithms/01. Arrays/00. Sorting/04. Quick Sort/Quick_Sort.java
@@ -10,7 +10,37 @@ public class Quick_Sort {
 		System.out.println("Array before Sorting: " + Arrays.toString(arr));
 
 		int n = arr.length;
-		
+		quickSort(arr, 0, n - 1);
 		System.out.println("Array after Sorting: " + Arrays.toString(arr));
+	}
+
+	public static void quickSort(int arr[], int l, int h) {
+		if (l < h) {
+			int pivotIndex = partition(arr, l, h);
+			quickSort(arr, l, pivotIndex - 1);
+			quickSort(arr, pivotIndex + 1, h);
+		}
+	}
+
+	private static int partition(int arr[], int l, int h) {
+		int pivot = arr[l];
+		int i = l;
+		int j = h;
+		while (i < j) {
+			while (i <= h && arr[i] <= pivot)
+				i++;
+			while (j >= l && arr[j] > pivot)
+				j--;
+			if (i < j)
+				swap(arr, i, j);
+		}
+		swap(arr, j, l);
+		return j;
+	}
+
+	static void swap(int arr[], int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
 }
