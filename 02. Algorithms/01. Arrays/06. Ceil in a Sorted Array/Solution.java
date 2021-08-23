47f163c627fa475b4617931887e018221b14e6d7
Pranay Gupta <64855541+thepranaygupta@users.noreply.github.com>
2021-08-24 02:24:43 +0530

    Create Solution.java

---
+++ 02. Algorithms/01. Arrays/06. Ceil in a Sorted Array/Solution.java
@@ -0,0 +1,22 @@
+class Solution{
+    
+    // Function to find floor of x
+    // arr: input array
+    // n is the size of array
+    static int findFloor(long arr[], int n, long target){
+    
+        int start = 0;
+        int end = n - 1;
+	    while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if(arr[mid] == target)
+                return mid;
+            else if (target > arr[mid]) {
+	    		start = mid + 1;
+	    	} else if (target < arr[mid]) {
+	    		end = mid - 1;
+	    	}
+	    }
+        return start;
+	}
+}
