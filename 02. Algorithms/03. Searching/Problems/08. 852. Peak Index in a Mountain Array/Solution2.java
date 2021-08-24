5a05437f77e0684e84ddead79247185577621650
Pranay Gupta <64855541+thepranaygupta@users.noreply.github.com>
2021-08-24 20:47:29 +0530

    Create Solution2.java

---
+++ 02. Algorithms/03. Searching/Problems/08. 852. Peak Index in a Mountain Array/Solution2.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/
+// using binary search
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end) {
+            int mid = start + (end - start)/2;
+            if(arr[mid] > arr[mid + 1]) {
+                // you're in decreasing part of the array
+                // arr[mid] may be the answer, but look at the left of mid
+                end = mid;
+            } else {
+                // you're in ascending part of the array
+                // because we know that arr[mid+1] > arr[mid]
+                start = mid + 1;
+            }
+        }
+        // in the end start == end point to the peak of the mountain
+        return start; // return start or end since both are same
+    }
+}
