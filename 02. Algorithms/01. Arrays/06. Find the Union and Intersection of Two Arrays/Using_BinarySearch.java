2c3d4bdc69a92a824a83a176f138871dc17563a9
Pranay Gupta <pranayguptamain@gmail.com>
2021-09-02 00:25:50 +0530

    Create Using_BinarySearch.java

---
+++ 02. Algorithms/01. Arrays/06. Find the Union and Intersection of Two Arrays/Using_BinarySearch.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        Arrays.sort(nums2);
+        for (Integer num : nums1) {
+            if (binarySearch(nums2, num)) {
+                set.add(num);
+            }
+        }
+        int i = 0;
+        int[] result = new int[set.size()];
+        for (Integer num : set) {
+            result[i++] = num;
+        }
+        return result;
+    }
+    
+    public boolean binarySearch(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return false;
+    }
+}
