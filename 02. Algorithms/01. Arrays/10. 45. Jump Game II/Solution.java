9589576f69a8a891b8feea8815eb0fc26eea8544
Pranay Gupta <pranayguptamain@gmail.com>
2021-09-07 20:37:51 +0530

    add: solution

---
+++ 02. Algorithms/01. Arrays/10. 45. Jump Game II/Solution.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/jump-game-ii/
+class Solution {
+    public int jump(int[] A) {
+        int jumps = 0, curEnd = 0, curFarthest = 0;
+        for (int i = 0; i < A.length - 1; i++) {
+            curFarthest = Math.max(curFarthest, i + A[i]);
+            if (i == curEnd) {
+                jumps++;
+                curEnd = curFarthest;
+            }
+        }
+        return jumps;
+    }
+}
