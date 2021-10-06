ac2592fa9cf28c99dcb03bf436338ed38c3a0948
Yug-gurnani <yug.gurnani091@gmail.com>
2021-10-06 19:35:37 +0530

    Added Edit Distance Problem for love babbar sheet

---
+++ Love Babbar DSA Sheet Solutions/Dynamic Programming/Edit Distance/Edit Distance.py
@@ -0,0 +1,52 @@
+"""
+Given two strings s and t. Find the minimum number of operations that need to be performed on str1 to convert it to str2.
+The possible operations are:
+
+Insert
+Remove
+Replace
+ 
+
+Example 1:
+
+Input: 
+s = "geek", t = "gesek"
+Output: 1
+Explanation: One operation is required 
+inserting 's' between two 'e's of str1.
+"""
+def editDistDP(str1, str2, m, n):
+    # Create a table to store results of subproblems
+    dp = [[0 for x in range(n + 1)] for x in range(m + 1)]
+ 
+    # Fill d[][] in bottom up manner
+    for i in range(m + 1):
+        for j in range(n + 1):
+ 
+            # If first string is empty, only option is to
+            # insert all characters of second string
+            if i == 0:
+                dp[i][j] = j    # Min. operations = j
+ 
+            # If second string is empty, only option is to
+            # remove all characters of second string
+            elif j == 0:
+                dp[i][j] = i    # Min. operations = i
+ 
+            # If last characters are same, ignore last char
+            # and recur for remaining string
+            elif str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+ 
+            # If last character are different, consider all
+            # possibilities and find minimum
+            else:
+                dp[i][j] = 1 + min(dp[i][j-1],        # Insert
+                                   dp[i-1][j],        # Remove
+                                   dp[i-1][j-1])    # Replace
+ 
+    return dp[m][n]
+"""
+TC = o(M * N)
+SC = o(M * N)
+"""
---
+++ Love Babbar DSA Sheet Solutions/Dynamic Programming/Edit Distance/README.md
@@ -0,0 +1,5 @@
+# Problem Name - Edit Distance problem
+# Problem Link - https://practice.geeksforgeeks.org/problems/edit-distance3702/1
+
+# TC = o(M * N)
+# SC = o(M * N)
