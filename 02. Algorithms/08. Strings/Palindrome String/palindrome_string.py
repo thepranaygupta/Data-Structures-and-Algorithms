7db5b6209f0485e5218e6e5aec59d50304dd0541
amarwade <amarwade@bol.com>
2021-10-22 11:22:26 +0200

    Added python code for checking a palindrome

---
+++ 02. Algorithms/08. Strings/Palindrome String/palindrome_string.py
@@ -0,0 +1,14 @@
+# The function to find if the string is a palindrome or not
+def is_palindrome(s):
+    s = s.lower()
+    return s == s[::-1]
+
+# The driver code
+if __name__ == '__main__':
+    s = str(raw_input("Ask user for something: "))
+    IS_PALINDROME = is_palindrome(s)
+
+    if IS_PALINDROME:
+        print("Yes, the input is a palindrome")
+    else:
+        print("No, the input in not a palindrome")
