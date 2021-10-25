8b6d1ec7af86a9024b192915b3205af26401a20f
Anmol Gupta <91716912+anamolguptaa@users.noreply.github.com>
2021-10-25 22:17:43 +0530

    Bottom view of a binary tree

---
+++ 01. DataStructures/04. Tree/Problems/Bottom-view-of-a-binary-tree.cpp
@@ -0,0 +1,32 @@
+vector<int> bottomview(TreeNode *root)
+{
+  vector<int> ans;
+  if (!root)
+    return ans;
+
+  map<int, int> map;
+  queue<pair<TreeNode *, int>> q;
+
+  q.push({root, 0});
+
+  while (!q.empty())
+  {
+    auto it = q.front();
+    q, pop();
+    TreeNode *node = it.first;
+    int line = it.second;
+    map[line] = node->val;
+
+    if (node->left)
+      q.push({node->left, line - 1});
+
+    if (node->right)
+      q.push({node->right, line + 1});
+  }
+
+  for (auto it : map)
+  {
+    ans.push_back(it.second);
+  }
+  return ans;
+}
