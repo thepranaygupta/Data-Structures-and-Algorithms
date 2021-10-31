b487a3ff1be47411cfc460ccdae4846987ffeef5
Souvik Mandal <souvikmandal210@gmail.com>
2021-10-31 13:19:49 +0530

    Update Fibonacci_SeriesUptoN.cpp

--- 02. Algorithms/04. Recursion/Problems/01. 509. Fibonacci Number/Fibonacci_SeriesUptoN.cpp
+++ 02. Algorithms/04. Recursion/Problems/01. 509. Fibonacci Number/Fibonacci_SeriesUptoN.cpp
@@ -1,16 +1,16 @@
-    //Fibonacci Series upto n numbers
+
     #include<iostream>
     using namespace std;
-    //main program
+
     int main()
     {
-    // initialising variables
+
     int limit, first=0, second=1, next, num;
     cout <<“Enter the limit of Fibonacci series”<<endl;
-    // user input
+
     cin >> num;   
     cout << “First “<<num<<” terms of Fibonacci series are :- “<<endl;
-    //loop for printing fibonacci series
+
     for(int p=0;p<num;p++)
     {
         if (p <= 1)
