3f91b5d5fa1be932a42f103303f0182ca2ac13d5
NIDHI CHAURASIA <77978770+NidhiChaurasia@users.noreply.github.com>
2021-10-31 10:24:50 +0530

    Created Heapsort.cpp
    Heap Sort implementation in C++ language.

---
+++ 02. Algorithms/02. Sorting/08. Heap Sort/HeapSort.cpp
@@ -0,0 +1,42 @@
+// Heap Sort in C++
+  #include <iostream>
+  using namespace std;
+  void heapify(int arr[], int n, int i) {
+    // Find largest among root, left child and right child
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+    if (left < n && arr[left] > arr[largest])
+      largest = left;
+    if (right < n && arr[right] > arr[largest])
+      largest = right;
+    // Swap and continue heapifying if root is not largest
+    if (largest != i) {
+      swap(arr[i], arr[largest]);
+      heapify(arr, n, largest);
+    }  }
+  // main function to do heap sort
+  void heapSort(int arr[], int n) {
+    // Build max heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+      heapify(arr, n, i);
+    // Heap sort
+    for (int i = n - 1; i >= 0; i--) {
+      swap(arr[0], arr[i]);
+      // Heapify root element to get highest element at root again
+      heapify(arr, i, 0);
+    }}
+  // Print an array
+  void printArray(int arr[], int n) {
+    for (int i = 0; i < n; ++i)
+      cout << arr[i] << " ";
+    cout << "\n";
+  }
+  // Driver code
+  int main() {
+    int arr[] = {1, 12, 9, 5, 6, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    heapSort(arr, n);
+    cout << "Sorted array is \n";
+    printArray(arr, n);
+  }
