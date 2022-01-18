657de2dca29ca75e2abeb7316cb7752cd0789826
ybrenning <yannickbrenning2@gmail.com>
2022-01-18 20:03:50 +0100

    Reformatting and removing redundant code

--- 01. DataStructures/01. Linked List/1. Singly-Linked-List/Basic Operations/SinglyLL.py
+++ 01. DataStructures/01. Linked List/1. Singly-Linked-List/Basic Operations/SinglyLL.py
@@ -1,65 +1,32 @@
 # A simple Python program to introduce a linked list
- 
+
 class Node:
- 
-    # Function to initialise the node object
+
+    # Function to initialize the node object
     def __init__(self, data):
         self.data = data  # Assign data
-        self.next = None  # Initialize next as null
- 
- 
+        self.next = None  # Initialize next as None
+
+
 # Linked List class contains a Node object
 class LinkedList:
- 
+
     # Function to initialize head
     def __init__(self):
         self.head = None
- 
- 
+
+
 # Code execution starts here
-if __name__=='__main__':
- 
+if __name__ == '__main__':
     # Start with the empty list
     llist = LinkedList()
- 
+
     llist.head = Node(1)
     second = Node(2)
     third = Node(3)
- 
-   
-    llist.head.next = second; # Link first node with second
-    second.next = third; # Link second node with the third node
- 
-  
- 
-# Node class
-class Node:
- 
-    # Function to initialise the node object
-    def __init__(self, data):
-        self.data = data  # Assign data
-        self.next = None  # Initialize next as null
- 
- 
-# Linked List class contains a Node object
-class LinkedList:
- 
-    # Function to initialize head
-    def __init__(self):
-        self.head = None
- 
- 
-# Code execution starts here
-if __name__=='__main__':
- 
-    # Start with the empty list
-    llist = LinkedList()
- 
-    llist.head = Node(1)
-    second = Node(2)
-    third = Node(3)
- 
- 
-    llist.head.next = second; # Link first node with second
-    second.next = third; # Link second node with the third node
- 
+
+    llist.head.next = second  # Link first node with second
+    second.next = third  # Link second node with the third node
+
+    # The linked list now looks as follows:
+    # head (1) -> second (2) -> third (3) -> None
