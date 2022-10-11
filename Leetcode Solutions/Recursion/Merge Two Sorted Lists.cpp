b3ec10b9cc1c2acb07bf2f6a329d6aae41791744
Yash Gupta <50226252+YashGupta018@users.noreply.github.com>
2022-10-11 18:24:43 +0530

    Added 3 More Ques.

---
+++ Leetcode Solutions/Recursion/Merge Two Sorted Lists.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode dummy(INT_MIN);
+        ListNode *tail = &dummy;
+        
+        while (l1 && l2) {
+            if (l1->val < l2->val) {
+                tail->next = l1;
+                l1 = l1->next;
+            } else {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+};
