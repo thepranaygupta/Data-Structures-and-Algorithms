ecf6ebc5e701ccb55f1443a3a4561928f558ee7b
Aadil <77232799+aadil42@users.noreply.github.com>
2022-10-16 18:23:48 +0530

    Create queue_stack.js
    Created queue using stacks in JS.

---
+++ 01. DataStructures/03. Queue/Basic Operations/03. Queue using Stacks/queue_stack.js
@@ -0,0 +1,103 @@
+// - Enqueue() â€“ Add item to the queue from the REAR.
+// - Dequeue() â€“ Remove item from the queue from the FRONT.
+// - isFull() â€“ Check if queue is full or not.
+// - isEmpty() â€“ Check if queue empty or not.
+// - count() â€“ Get number of items in the queue.
+// - peek() â€“ return front elemenet in the queue(line).
+// 1 2 3 4 5 6 7 8 9 10
+
+
+class Stack {
+    constructor(size){
+        this.stack = [];
+    }
+    
+    isEmpty(){
+        return this.stack.length === 0 ? true : false;
+    }
+    push(data) {
+        return this.stack.push(data);
+    }
+    pop(){
+        return this.stack.pop();
+    }
+    size() {
+        return this.stack.length;
+    }
+}
+
+class Queue {
+    constructor(size) {
+        this.stackA = new Stack();
+        this.stackB = new Stack();
+        this.size = size;
+        // [2,3,4,5,6]
+        // [] []
+    }
+
+    enqueu(data) {
+        // console.log(this.stackA.size(), this.stackB.size(), this.size);
+        if(this.stackA.size() + this.stackB.size() < this.size) {
+            this.stackA.push(data);
+        }
+    }
+
+    dequeue() {
+        if(this.stackB.isEmpty()) {
+            while(!this.stackA.isEmpty()) {
+                this.stackB.push(this.stackA.pop());
+            }
+        }
+
+        this.stackB.pop();
+    }
+
+    peek() {
+        if(this.stackB.isEmpty()) {
+            return this.stackA.stack[0];
+        } else {
+            return this.stackB.stack[this.stackB.stack.length - 1];
+        }
+    }
+
+    isEmpty() {
+        if(this.stackA.isEmpty() && this.stackB.isEmpty()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    isFull() {
+        if(this.stackA.isEmpty() && this.stackB.isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    count() {
+        return this.stackA.size() + this.stackB.size();
+    }
+}
+
+// 3,10,101,52,4
+const myQueue = new Queue(5);
+myQueue.enqueu(1); 
+myQueue.enqueu(2);
+myQueue.enqueu(3);
+myQueue.enqueu(4);
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.dequeue();
+myQueue.enqueu(5);
+myQueue.enqueu(5);
+myQueue.enqueu(5);
+myQueue.enqueu(5);
+myQueue.enqueu(5);
+myQueue.dequeue(5);
+myQueue.enqueu(5);
+
+console.log(myQueue.count());
+
