341033a4cb865b34cd8ab9c36a37c9c5d3b2a3e1
Aadil <77232799+aadil42@users.noreply.github.com>
2023-05-30 15:15:18 +0530

    Create pascalsTriangle.js
    Added solution in JS.
    
    Language: JS
    
    Submission link: https://leetcode.com/problems/pascals-triangle/submissions/828733368/

---
+++ 02. Algorithms/01. Arrays/17. Pascal's Triangle/pascalsTriangle.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+
+// the time complexity will basically be the number of element in pascale tringle.
+// O(n^2);
+
+var generate = function(num) {
+    
+    const outerArray = [];
+    // adding first two rows of pascals triangle
+    if (num >= 2) {
+      outerArray.push([1]);
+      outerArray.push([1, 1]);
+    } else {
+      outerArray.push([1]);
+    }
+  
+    // will  only run if we had number greater than 2
+    if (num > 2) {
+      for (let i = 2; i < num; i++) {
+        let subArray = [];
+         subArray.push(1);
+        for (let j = 0; j < outerArray[i - 1].length - 1; j++) {
+          subArray.push(outerArray[i - 1][j] + outerArray[i - 1][j + 1]);
+        }
+        subArray.push(1);
+        outerArray.push(subArray);
+      }
+    }
+  
+    return outerArray;
+ };
