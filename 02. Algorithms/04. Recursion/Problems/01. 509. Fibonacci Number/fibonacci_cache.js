06fb68e5cdc80b4f3b8370070c9ac7e2cf832c0a
Aadil <77232799+aadil42@users.noreply.github.com>
2023-07-05 20:04:47 +0530

    Create fibonacci_cache.js
    Solved fibonacci with caching.
    
    submission link: https://leetcode.com/problems/fibonacci-number/submissions/987006426/

---
+++ 02. Algorithms/04. Recursion/Problems/01. 509. Fibonacci Number/fibonacci_cache.js
@@ -0,0 +1,23 @@
+/**
+ * Time O(n) | Space O(n)
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    
+    const hash = {};
+    const dfs = (n) => {
+        if(hash[n]) return hash[n];
+        if(n <= 0) {
+            return 0;
+        }
+        if(n === 1) {
+            return 1;
+        }
+        const result =  dfs(n-1) + dfs(n-2);
+        hash[n] = result;
+        return result;
+    }
+
+    return dfs(n);
+};
